3de90ca4bc9f7d031dc614853ad002ff7340f6cb
CHOIJIONE\choij <choijione02@gmail.com>
2025-12-28 18:25:01 +0900

    게임 시작 시 커서 Hide하고, 종료하면 Show하는 기능 추가

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -10,6 +10,8 @@ int main(void)
 	ConsoleManager::Get().Startup();
 	InputManager::Get().Startup();
 
+	ConsoleManager::Get().SetVisibleCursor(false);
+
 	GameContext context;
 	
 	int32_t x = 10;
@@ -19,29 +21,35 @@ int main(void)
 	context.SetTile(x, y, ETile::HEAD);
 
 	bool isDone = false;
+	bool isPressed = false;
 	while (!isDone)
 	{
+		isPressed = false;
 		int32_t newX = x;
 		int32_t newY = y;
 
 		if (HIBYTE(GetAsyncKeyState(VK_DOWN)))
 		{
+			isPressed = true;
 			newY++;
 		}
 		if (HIBYTE(GetAsyncKeyState(VK_UP)))
 		{
+			isPressed = true;
 			newY--;
 		}
 		if (HIBYTE(GetAsyncKeyState(VK_LEFT)))
 		{
+			isPressed = true;
 			newX--;
 		}
 		if (HIBYTE(GetAsyncKeyState(VK_RIGHT)))
 		{
+			isPressed = true;
 			newX++;
 		}
 
-		if (context.IsValidTile(newX, newY))
+		if (context.IsValidTile(newX, newY) &&  isPressed)
 		{
 			const ETile& tile = context.GetTile(x, y);
 			
@@ -54,6 +62,8 @@ int main(void)
 
 		context.Render();
 	}
+
+	ConsoleManager::Get().SetVisibleCursor(true);
 
 	InputManager::Get().Shutdown();
 	ConsoleManager::Get().Shutdown();
