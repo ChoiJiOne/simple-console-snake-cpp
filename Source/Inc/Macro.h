8c068b598033f6f43f3f9a456db2f8b6cb9e6503
CHOIJIONE\choij <choijione02@gmail.com>
2025-12-28 17:23:54 +0900

    기존의 매크로 선언 파일 삭제

--- Source/Inc/Macro.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef DISALLOW_COPY_AND_ASSIGN
-#define DISALLOW_COPY_AND_ASSIGN(TypeName)    \
-TypeName(TypeName&&) = delete;                \
-TypeName(const TypeName&) = delete;           \
-TypeName& operator=(TypeName&&) = delete;     \
-TypeName& operator=(const TypeName) = delete;
-#endif
