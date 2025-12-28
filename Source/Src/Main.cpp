11177f329f2ebb2daabdf75202253ab426d6b44d
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-12-28 11:11:29 +0900

    뱀이 돌아다닐 보드 렌더링 테스트 코드 추가

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -2,5 +2,21 @@
 
 int main(void)
 {
+	for (int y = 0; y < 10; ++y)
+	{
+		for (int x = 0; x < 10; ++x)
+		{
+			if (x == 0 || x == 9 || y == 0 || y == 9)
+			{
+				std::cout << "#";
+			}
+			else
+			{
+				std::cout << " ";
+			}
+		}
+		std::cout << "\n";
+	}
+
 	return 0;
 }
