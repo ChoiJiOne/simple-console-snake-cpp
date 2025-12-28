bbb3aaa8ca3470cbcefbfe3b8bbe5577e1b78b4d
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-12-28 11:36:36 +0900

    게임 컨텍스트에 타일 목록 초기화 및 콘솔 화면에 표시 기능 추가

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -1,42 +1,90 @@
+#include <cassert> // 임시로 기본적으로 제공하는 assert 사용.
 #include <iostream>
+#include <map>
+#include <vector>
 
-// TODO: 임시로 클래스가 아닌 구조체로 선언 (나중에 class로 변경)
-struct GameContext
+enum class ETile
 {
-	int32_t board[10][10] = { 0, };
+	EMPTY = 0x00,
+	WALL = 0x01,
+};
+
+class GameContext
+{
+public:
+	GameContext()
+	{
+		_tiles = std::vector<ETile>(_rowSize * _colSize, ETile::EMPTY);
+		_tileCharMap = 
+		{
+			{ ETile::EMPTY, ' ' },
+			{ ETile::WALL, '#' },
+		};
+
+		for (int y = 0; y < _rowSize; ++y)
+		{
+			for (int x = 0; x < _colSize; ++x)
+			{
+				if (IsOutline(x, y))
+				{
+					SetTile(x, y, ETile::WALL);
+				}
+			}
+		}
+	}
+
+	virtual ~GameContext() { }
+
+	void Render()
+	{
+		for (int y = 0; y < _rowSize; ++y)
+		{
+			for (int x = 0; x < _colSize; ++x)
+			{
+				int32_t offset = y * _colSize + x;
+
+				auto it = _tileCharMap.find(_tiles[offset]);
+				if (it != _tileCharMap.end())
+				{
+					std::cout << it->second;
+				}
+			}
+
+			std::cout << "\n";
+		}
+	}
+
+private:
+	bool IsOutline(int32_t x, int32_t y)
+	{
+		return (x <= 0 || x >= _colSize - 1) || (y <= 0 || y >= _rowSize - 1);
+	}
+
+	bool IsValidTile(int32_t x, int32_t y)
+	{
+		return (0 <= x && x <= _colSize - 1) && (0 <= y || y <= _rowSize - 1);
+	}
+
+	void SetTile(int32_t x, int32_t y, const ETile& tile)
+	{
+		assert(IsValidTile(x, y));
+
+		int32_t offset = y * _colSize + x;
+		_tiles[offset] = tile;
+	}
+
+private:
+	int32_t _rowSize = 10; // 세로 크기
+	int32_t _colSize = 10; // 가로 크기
+
+	std::vector<ETile> _tiles;
+	std::map<ETile, char> _tileCharMap;
 };
 
 int main(void)
 {
 	GameContext context;
-
-	for (int y = 0; y < 10; ++y)
-	{
-		for (int x = 0; x < 10; ++x)
-		{
-			if (x == 0 || x == 9 || y == 0 || y == 9)
-			{
-				context.board[x][y] = 1;
-			}
-		}
-	}
-
-	for (int y = 0; y < 10; ++y)
-	{
-		for (int x = 0; x < 10; ++x)
-		{
-			if (context.board[x][y] == 1)
-			{
-				std::cout << "#";
-			}
-			else
-			{
-				std::cout << " ";
-			}
-		}
-
-		std::cout << "\n";
-	}
+	context.Render();
 
 	return 0;
 }
