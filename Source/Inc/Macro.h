5ad079f8ec5365617bc7be584a49227c7f96720e
CHOIJIONE\choij <choijione02@gmail.com>
2025-12-28 17:13:01 +0900

    복사 생성자 및 대입 연산자를 삭제하는 매크로 추가

---
+++ Source/Inc/Macro.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef DISALLOW_COPY_AND_ASSIGN
+#define DISALLOW_COPY_AND_ASSIGN(TypeName)    \
+TypeName(TypeName&&) = delete;                \
+TypeName(const TypeName&) = delete;           \
+TypeName& operator=(TypeName&&) = delete;     \
+TypeName& operator=(const TypeName) = delete;
+#endif
