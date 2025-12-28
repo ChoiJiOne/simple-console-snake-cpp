2610f7593050e3197241bd38dae1a046323c7378
CHOIJIONE\choij <choijione02@gmail.com>
2025-12-29 08:08:58 +0900

    테스트용 코드 삭제

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -1,5 +1,3 @@
-#include <Windows.h>
-
 #include "ConsoleManager.h"
 #include "InputManager.h"
 
@@ -15,87 +13,53 @@ int main(void)
 
 	GameContext context;
 
+	int32_t x = 10;
+	int32_t y = 10;
+
+	// 임시 테스트용 코드.
+	context.SetTile(x, y, ETile::HEAD);
+
 	bool isDone = false;
 	while (!isDone)
 	{
+		int32_t newX = x;
+		int32_t newY = y;
+
 		InputManager::Get().Tick();
 
-		EPress press = InputManager::Get().GetKeyPress(EKey::SPACE);
-		if (press == EPress::NONE)
-		{
-			ConsoleManager::Get().Print(0, 0, "NONE!!!");
+		if (InputManager::Get().GetKeyPress(EKey::LEFT) == EPress::PRESSED)
+		{	
+			newX--;
 		}
-		if (press == EPress::PRESSED)
+
+		if (InputManager::Get().GetKeyPress(EKey::RIGHT) == EPress::PRESSED)
 		{
-			ConsoleManager::Get().Print(0, 0, "PRESSED!!!");
+			newX++;
 		}
-		if (press == EPress::HELD)
+
+		if (InputManager::Get().GetKeyPress(EKey::UP) == EPress::PRESSED)
 		{
-			ConsoleManager::Get().Print(0, 0, "HELD!!!");
+			newY--;
 		}
-		if (press == EPress::RELEASED)
+
+		if (InputManager::Get().GetKeyPress(EKey::DOWN) == EPress::PRESSED)
 		{
-			ConsoleManager::Get().Print(0, 0, "RELEASED!!!");
+			newY++;
 		}
+
+		if (context.IsValidTile(newX, newY) && !context.IsOutline(newX, newY))
+		{
+			const ETile& tile = context.GetTile(x, y);
+		
+			context.SetTile(x, y, ETile::EMPTY);
+
+			x = newX;
+			y = newY;
+			context.SetTile(x, y, ETile::HEAD);
+		}
+
+		context.Render();
 	}
-
-	//int32_t x = 10;
-	//int32_t y = 10;
-
-	//// 임시 테스트용 코드.
-	//context.SetTile(x, y, ETile::HEAD);
-
-	//bool isDone = false;
-	//bool isPressed = false;
-	//while (!isDone)
-	//{
-	//	isPressed = false;
-	//	int32_t newX = x;
-	//	int32_t newY = y;
-
-	//	if (HIBYTE(GetAsyncKeyState(VK_DOWN)))
-	//	{
-	//		isPressed = true;
-	//		newY++;
-	//	}
-	//	if (HIBYTE(GetAsyncKeyState(VK_UP)))
-	//	{
-	//		isPressed = true;
-	//		newY--;
-	//	}
-	//	if (HIBYTE(GetAsyncKeyState(VK_LEFT)))
-	//	{
-	//		isPressed = true;
-	//		newX--;
-	//	}
-	//	if (HIBYTE(GetAsyncKeyState(VK_RIGHT)))
-	//	{
-	//		isPressed = true;
-	//		newX++;
-	//	}
-
-	//	if (HIBYTE(GetAsyncKeyState(VK_ESCAPE)))
-	//	{
-	//		ConsoleManager::Get().Clear();
-	//	}
-	//	if (HIBYTE(GetAsyncKeyState(VK_SPACE)))
-	//	{
-	//		isPressed = true;
-	//	}
-
-	//	if (context.IsValidTile(newX, newY) &&  isPressed)
-	//	{
-	//		const ETile& tile = context.GetTile(x, y);
-	//		
-	//		context.SetTile(x, y, ETile::EMPTY);
-
-	//		x = newX;
-	//		y = newY;
-	//		context.SetTile(x, y, ETile::HEAD);
-	//	}
-
-	//	context.Render();
-	//}
 
 	ConsoleManager::Get().SetVisibleCursor(true);
 
